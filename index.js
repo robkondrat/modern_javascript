976b69f8426e995b38d5ec2adf36ac475d40faf4
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-17 11:44:34 -0600

    in class

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("Hello from JS");
 
-var datetime = moment().startOf('minute').fromNow();
+var datetime = moment().startOf('Day').fromNow();
 console.log(datetime);
+
+var number = 144;
+
+console.log("Ch Ch Ch Changes");
+var square_root = math.sqrt(number);
+
+
+var name = 'Bob';
+console.log(`Whatup ${name}, the day started ${datetime}. The square root of ${number} is ${square_root}.`);
+
